d91a07d7446b787c78250020e32e56aa1429ae57
Sharmina Akhtar <sakht318@gmail.com>
2017-11-15 11:54:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,7 @@
 def starts_with_a_vowel?(word)
-  if %w{word}
-    true
-  else /^[^aeiou\W]/i.match(s)
-    false
-  end
+   %w{word}
+     
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
