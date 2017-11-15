bc801ec8003a9e1ad99a7bba7ae9065bdf98d3c7
Sharmina Akhtar <sakht318@gmail.com>
2017-11-15 11:07:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if %w{word}
+    true
+  else !%w{word}
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ /un\ing$/{text} 
 end
 
 def words_five_letters_long(text)
